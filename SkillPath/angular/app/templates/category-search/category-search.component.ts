c145a7669049c4d857b7741816aa6a4b89254934
Levi Fuller <lfuller941@gmail.com>
2017-04-23 21:45:21 -0600

    Custom category search styling; categories connected to Api

--- SkillPath/angular/app/templates/category-search/category-search.component.ts
+++ SkillPath/angular/app/templates/category-search/category-search.component.ts
@@ -1,7 +1,9 @@
 import { Observable } from 'rxjs/Observable';
 import { Category } from './../../models/category';
 import { CategoryService } from './../../services/category.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import 'rxjs/add/observable/from';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-category-search',
@@ -11,21 +13,47 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CategorySearchComponent implements OnInit {
 
   constructor(private _categoryService: CategoryService) { }
+  @Input() title: string;
   @Input() tier: number;
-  private categories: Observable<Category[]>;
+  @Input() selectedCategory: Category = {};
+  @Output() selectedCategoryChange: EventEmitter<Category> = new EventEmitter<Category>();
+  private categories: Category[];
   ngOnInit() {
-    this.categories = this._categoryService.getCategories(this.tier);
+    this._categoryService.getCategories(this.tier).subscribe(categories => this.categories = categories);
   }
-   protected captains = ['James T. Kirk', 'Benjamin Sisko', 'Jean-Luc Picard', 'Spock', 'Jonathan Archer', 'Hikaru Sulu', 'Christopher Pike', 'Rachel Garrett' ];
 
-   selectItem(selectedItem) {
-    console.log(selectedItem);
-    this.selectedItem = selectedItem;
+  private filteredCategories: Category[];
+
+  onSelectedCategoryChanged() {
     
+  }
+  isFocused: boolean = false;
+  onFocused() {
+     this.isFocused = true;
+  }
+
+  onBlur() {
+    this.isFocused = false;
+  }
+
+  log(message) {
+    console.log(message);
+  }
+
+  filterCategories() {
+
+  }
+  
+   selectCategory(selectedCategoryId: string) {
+     //Observable.from(this.categories).filter(x => x.filter)
+     //this.selectedCategory = 
+     this.categories.forEach(cat => {
+       if (cat.id == selectedCategoryId) {
+        this.selectedCategory = cat;
+        this.selectedCategoryChange.emit(this.selectedCategory);
+       }
+      });
+      
    }
-   selectedItem: string = "";
-   filterCategories(value: string) {
-    
-    
-   }
+  
 }
