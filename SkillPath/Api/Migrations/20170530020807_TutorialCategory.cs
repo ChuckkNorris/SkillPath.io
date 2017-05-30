c3e43f4863f47e84ee98a4a80d1578cb22b1ed68
Levi Fuller <lfuller941@gmail.com>
2017-05-29 20:21:19 -0600

    Added Tutorial entity and many-many relationship to categories

---
+++ SkillPath/Api/Migrations/20170530020807_TutorialCategory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SkillPath.api.Migrations
+{
+    public partial class TutorialCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tutorials",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    LinkUrl = table.Column<string>(nullable: true),
+                    Tier = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tutorials", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TutorialCategories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CategoryId = table.Column<Guid>(nullable: false),
+                    TutorialId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TutorialCategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TutorialCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TutorialCategories_Tutorials_TutorialId",
+                        column: x => x.TutorialId,
+                        principalTable: "Tutorials",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TutorialCategories_CategoryId",
+                table: "TutorialCategories",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TutorialCategories_TutorialId",
+                table: "TutorialCategories",
+                column: "TutorialId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TutorialCategories");
+
+            migrationBuilder.DropTable(
+                name: "Tutorials");
+        }
+    }
+}
