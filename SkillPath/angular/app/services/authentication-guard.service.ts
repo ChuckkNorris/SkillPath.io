24d9c2c1e7654c9595c1fbd036397abfda4ff98b
Levi Fuller <lfuller941@gmail.com>
2017-06-21 21:26:40 -0600

    Added redirect if not authenticated

--- SkillPath/angular/app/services/authentication-guard.service.ts
+++ SkillPath/angular/app/services/authentication-guard.service.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
@@ -5,9 +6,14 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angul
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
-    constructor(private _authService: AuthenticationService) {}
+    constructor(private _authService: AuthenticationService, private router: Router) {}
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
-        return this._authService.isAuthenticated;
+        let toReturn = this._authService.isAuthenticated;
+        if (!toReturn) {
+            this.router.navigate(['login']);
+        }
+        
+        return toReturn;
     }
 
 
