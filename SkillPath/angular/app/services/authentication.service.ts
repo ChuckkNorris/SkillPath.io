450eb0b5e412a01fea41b486e80f8a2f64634104
Levi Fuller <lfuller941@gmail.com>
2017-06-26 22:31:58 -0600

    Added logo and cookie storage

--- SkillPath/angular/app/services/authentication.service.ts
+++ SkillPath/angular/app/services/authentication.service.ts
@@ -1,18 +1,26 @@
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CookieService } from "ngx-cookie-service";
+
+const BEARER_TOKEN = 'bearer_token';
 
 @Injectable()
 export class AuthenticationService {
-    
+
+    constructor(
+        private _cookieService: CookieService
+    ) {}
+
     private _isAuthenticated : boolean;
     public get isAuthenticated() : boolean {
-        return this._isAuthenticated;
+        return this._cookieService.get(BEARER_TOKEN) == 'tokenHere';
     }
     
     public login(username: string, password: string) {
         if (username == "commander" && password == "cobra1234") {
             this._isAuthenticated = true;
+            this._cookieService.set(BEARER_TOKEN, 'tokenHere');
         }
         return this.isAuthenticated;
     }
