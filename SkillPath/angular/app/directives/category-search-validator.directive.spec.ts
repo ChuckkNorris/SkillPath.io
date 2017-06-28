a61a595586a230400b628f6e4f478830eb8a5981
Levi Fuller <lfuller941@gmail.com>
2017-06-27 21:28:47 -0600

    Updated CategorySearchComponent to utilize ngModel; validators in progress

---
+++ SkillPath/angular/app/directives/category-search-validator.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CategorySearchValidatorDirective } from './category-search-validator.directive';
+
+describe('CategorySearchValidatorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CategorySearchValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
