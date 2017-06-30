138e0f62a5552b40fec9633c88fb0fcc003bff0b
Levi Fuller <lfuller941@gmail.com>
2017-06-29 22:30:54 -0600

    Made Angular parent fields readonly; tutorial form validation working; awaiting release of https://github.com/angular/flex-layout/issues/270 in order to fxLayoutAlign center with hidden fields

--- SkillPath/angular/app/stagger-gap.directive.ts
+++ SkillPath/angular/app/stagger-gap.directive.ts
@@ -26,36 +26,6 @@ export class StaggerGapDirective {
     this.setMarginTop();
   }
 
-  getMarginTop() {
-    let toReturn: number; 
-    if (this.isOnNextRow()) {
-      toReturn = -(this.getTotalHeight(this.aboveElement)/2) - this.aboveElement.nativeElement.style.topMargin + this.aboveElement.nativeElement.style.botMargin;
-    }
-    return toReturn;
-  }
-
-  setHeight() {
-    let firstChild = this.flexElement.nativeElement.children[0];
-    // console.log(firstChild);
-    if (firstChild.style.height) {
-      // Set height to match content
-      let firstChildTotalHeight = this.getTotalHeight(firstChild);
-      console.log('My height is ' + this.flexElement.nativeElement.style.height);
-      console.log('First Child height is: ' + firstChildTotalHeight);
-      
-      this.flexElement.nativeElement.style.height = firstChildTotalHeight;
-    }
-  }
-
-  // 
-  
-
-  getTotalHeight(element): number {
-    return element.offsetHeight;
-    //  return element.style.height + element.style.paddingTop + element.style.paddingBottom + 
-    //     element.style.marginTop + element.style.marginBottom;
-  }
-
   setMarginTop() {
     // COUNT PER ROW: 3
     // ABOVE: row=2, column=2, index=4 
@@ -64,14 +34,16 @@ export class StaggerGapDirective {
     if (this.isOnNextRow()) {
       let countPerRow = this.getItemCountPerRow();
       let parentIndex = this.index - countPerRow;
-      let parentEl = this.flexElement.nativeElement.parentElement.children[parentIndex];
+      let parent = this.flexElement.nativeElement.parentElement;
+      if (parent.children) {
+        let parentEl = parent.children[parentIndex];
       
-      var parentHeight = parentEl.offsetHeight;
-      var parentCardHeight = parentEl.children[0].offsetHeight;
-      var diff = parentHeight - parentCardHeight;
-      console.log('index: ' + this.index + ', parentHeight: ' + parentHeight + ', parentIndex: ' + parentIndex);
-      //newMarginTop = -(parentHeight/2) - 20;
-      newMarginTop = -diff + 20
+        let parentHeight = parentEl.offsetHeight;
+        let parentCardHeight = parentEl.children[0].offsetHeight;
+        let diff = parentHeight - parentCardHeight;
+        //newMarginTop = -(parentHeight/2) - 20;
+        newMarginTop = -diff + 20;
+      }
     }
     else 
       newMarginTop = 0;
@@ -80,14 +52,6 @@ export class StaggerGapDirective {
 
   getItemCountPerRow() {
     let toReturn;
-    // let styles = this.flexElement.nativeElement.style.cssText.split(';');
-    // let maxWidthPercent = "1";
-    // styles.forEach(style => {
-    //   let nameValue = style.split(':');
-    //   if (nameValue[0].trim() == "max-width") {
-    //     maxWidthPercent = (nameValue[1] as string).substring(0, nameValue[1].indexOf('%'));
-    //   }
-    // });
      let maxWidthPercentStyle = this.flexElement.nativeElement.style.maxWidth;
      let maxWidth = maxWidthPercentStyle.substring(0, maxWidthPercentStyle.indexOf('%'));
     toReturn = Math.floor(100 / parseFloat(maxWidth));
