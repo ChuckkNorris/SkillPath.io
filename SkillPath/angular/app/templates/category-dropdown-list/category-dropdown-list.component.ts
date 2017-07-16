c82569f50bca45f4bfddff1cc311f109efba241a
Levi Fuller <lfuller941@gmail.com>
2017-07-16 13:24:09 -0600

    hiding child category dropdowns when parent dropdown category is not selected

--- SkillPath/angular/app/templates/category-dropdown-list/category-dropdown-list.component.ts
+++ SkillPath/angular/app/templates/category-dropdown-list/category-dropdown-list.component.ts
@@ -1,6 +1,6 @@
 import { Category } from './../../models/category';
 import { CategoryService } from './../../services/category.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-category-dropdown-list',
@@ -11,7 +11,7 @@ export class CategoryDropdownListComponent implements OnInit {
 
   constructor(private _categoryService: CategoryService) { }
   
-
+  @Input() showEmptyCategories: boolean = true;
   t1Categories: Category[] = [];
   selectedT1Category: Category;
   t2Categories: Category[] = [];
@@ -25,14 +25,14 @@ export class CategoryDropdownListComponent implements OnInit {
   }
 
   getCategories(tier: number) {
-    this._categoryService.getCategories(tier).subscribe(cats => {
+    this._categoryService.getCategories(tier, this.showEmptyCategories).subscribe(cats => {
       this.t1Categories = cats;
     });
   }
 
   getChildCategories(categoryListName: string, parentId: string) {
     this.deselectChildCategories(+categoryListName[1]);
-    this._categoryService.getChildCategories(parentId).subscribe(cats => {
+    this._categoryService.getChildCategories(parentId, this.showEmptyCategories).subscribe(cats => {
       this[categoryListName] = cats;
     });
   }
